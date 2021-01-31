22c00deafe611a57761582e797e19da30966f761
Dev_Khatab <khatap1@hotmail.com>
2021-01-30 21:41:37 -0800

    Editing the Entites to fit with Requirements

---
+++ LibrarySystem/LibrarySystem.Api/Migrations/20210131054019_Edit_Enities.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibrarySystem.Api.Migrations
+{
+    public partial class Edit_Enities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MainCategory",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubCategory",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MainCategory",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "SubCategory",
+                table: "Books");
+        }
+    }
+}
