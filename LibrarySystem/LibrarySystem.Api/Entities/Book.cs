5088a9d6b303312615c77257bd31b5eded016a86
Dev_Khatab <khatap1@hotmail.com>
2021-01-30 21:18:26 -0800

    Adding Resource Prameters For Filtering Reasons,Edit the Book Enitity

--- LibrarySystem/LibrarySystem.Api/Entities/Book.cs
+++ LibrarySystem/LibrarySystem.Api/Entities/Book.cs
@@ -9,6 +9,8 @@ namespace LibrarySystem.Api.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string MainCategory { get; set; }
+        public string SubCategory { get; set; }
         public Author Author { get; set; }
         public Guid AuthorId { get; set; }
     }
---
+++ LibrarySystem/LibrarySystem.Api/ResourceParameter/BookResourceParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Api.ResourceParameter
+{
+    public class BookResourceParameter
+    {
+        public string MainCategory { get; set; }
+        public string SubCategory { get; set; }
+    }
+}
