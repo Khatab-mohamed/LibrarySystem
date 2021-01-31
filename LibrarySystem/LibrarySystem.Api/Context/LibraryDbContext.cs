a8535b448fb443b68676de2c77c8bdfaf8326fb8
Dev_Khatab <khatap1@hotmail.com>
2021-01-30 20:25:04 -0800

    Adding the DbContext, Configuring Sql Server

---
+++ LibrarySystem/LibrarySystem.Api/Context/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LibrarySystem.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySystem.Api.Context
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+    }
+}
