6e1fb97dbf3d7bcfaa52418e4c29bb8798c45cd5
Dev_Khatab <khatap1@hotmail.com>
2021-01-30 19:58:12 -0800

    Adding the models of the Entites

---
+++ LibrarySystem/LibrarySystem.Api/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Api.Entities
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Author Author { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
