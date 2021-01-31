fc69e7e30a305caa0ed76c8612f8554c16355f3b
Dev_Khatab <khatap1@hotmail.com>
2021-01-30 21:43:25 -0800

    Adding Web Api controller with Single  method retrieving data

---
+++ LibrarySystem/LibrarySystem.Api/Controllers/BooksController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibrarySystem.Api.Entities;
+using LibrarySystem.Api.Interfaces;
+using LibrarySystem.Api.ResourceParameter;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarySystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksRepository _booksRepository;
+
+        public BooksController(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+
+        [HttpGet]
+        [HttpHead]
+        public ActionResult<IEnumerable<Book>> GetAuthors(
+            [FromQuery] BookResourceParameter bookResourceParameter)
+        {
+            var books = _booksRepository.GetBooks(bookResourceParameter);
+            
+            return Ok(books);
+        }
+    }
+}
